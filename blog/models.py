9c2ff546798a95bfad24835187d73ddfe1f4357e
Sneha Mathadawar <mathadawar01@gmail.com>
2021-10-13 21:48:56 +0530

    Update File

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,20 @@
 from sqlalchemy import Column, Integer, String
 from .database import Base
 
+"""this Base is from database connection that is from database.py"""
+
 
 class Blog(Base):
     __tablename__ = 'blogs'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)  # id will be generated automatically
     title = Column(String)
     body = Column(String)
+
+
+class User(Base):  # base means database
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
